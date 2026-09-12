2677321d002bc00e978b071187c13de338be5b2b
Muhammad Ziaur Rahman <ziaur.contact@gmail.com>
2026-09-12 19:58:19 +0600

    Banner Added

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,42 @@
+import banner from "../assets/banner-stack.png";
+
 const Banner = () => {
   return (
-    <section
-      className="container mx-auto min-h-100
-      flex justify-center items-center "
-    >
-      <h3 className="text-3xl text-red-700">Banner</h3>
+    <section className="md:mt-0 mt-4 container mx-auto grid grid-cols-12 items-center">
+      <div className="col-span-12 md:col-span-6 flex flex-col gap-6 md:gap-10 md:items-start items-center">
+        <div className="font-bold md:text-5xl text-4xl flex flex-col gap-px md:items-start items-center">
+          <h1 className=" text-[#0F172A] text-center md:text-left">
+            Build Your Ideal
+          </h1>
+          <h1 className="bg-brand bg-clip-text text-transparent pb-1 text-center md:text-left">
+            Development Stack
+          </h1>
+        </div>
+
+        <p className="text-[#475569] md:text-left text-center">
+          Explore frontend, backend, database, and tooling options, <br />
+          compare them side by side, and put together the stack that fits your{" "}
+          <br />
+          next project.
+        </p>
+
+        <div className="flex gap-4 justify-center">
+          <button className="px-3 py-2 bg-brand-button font-semibold border-0 rounded-[10px] cursor-pointer">
+            Explore Technologies
+          </button>
+          <button className="cursor-pointer font-semibold px-6 md:px-16 border-[0.25px] rounded-[10px] border-common-border bg-white text-[#374151]">
+            Learn More
+          </button>
+        </div>
+      </div>
+
+      <div className="col-span-12 md:col-span-6 flex md:justify-end justify-center items-center mt-0">
+        <img
+          src={banner}
+          alt="banner image"
+          className="md:object-cover object-fill object-center"
+        />
+      </div>
     </section>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,8 @@ import logo from "../assets/logo-text.png";
 
 const NavBar = () => {
   return (
-    <nav className="sticky top-0 z-50 bg-[#F8FAFC] border-b-[0.05px] border-slate-200">
-      <div className="flex justify-between items-center py-4 container mx-auto">
+    <nav className="sticky py-4 top-0 z-50 bg-[#F8FAFC] border-b-[0.05px] border-slate-200">
+      <div className="flex justify-between items-center container mx-auto">
         <img src={logo} alt="navbar logo" />
         <ul className="flex justify-between items-center text-[#334155] gap-4">
           <li>
